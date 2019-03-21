ede53ca7c91d2b86692d08d409e481fb483cb57c
yike1 <2098933249@qq.com>
2019-03-21 22:12:23 +0800

    home 前端

--- src/client/components/Home.vue
+++ src/client/components/Home.vue
@@ -6,7 +6,29 @@
   			<p class="some" v-html="msg"></p>
   		</div>
   	</div>
-     <router-link to="/home">Goto Ap</router-link>
+    <div class="content">
+        <ul>
+            <li v-for="html in htmls">
+                <section class="cont-inf">
+                    <div class="icon">
+                        <img v-if="html.icon === '1'" src="../static/images/icon/artic.png">
+                        <img v-else-if="html.icon === '2'" src="../static/images/icon/design.png">
+                        <img v-else-if="html.icon === '3'" src="../static/images/icon/photograp.png">
+                        <img v-else-if="html.icon === '4'" src="../static/images/icon/note.png">
+                    </div>
+                    <p class="date">{{html.date}}</p>
+                    <p class="introduce">{{html.introduce}}</p>
+                </section>
+                <div class="photo-div">
+                    <div class="photo-img-div">
+                        <img :src="html.photo" class="photo-img"/>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <img src="../static/images/icon/artic.png">
+        <router-link to="/home">Goto Ap</router-link>
   </div>
 </template>
 <script type="text/javascript" src="../static/js/home.js"></script>
--- src/client/static/js/home.js
+++ src/client/static/js/home.js
@@ -1,8 +1,35 @@
+var arr = [
+                {
+                    icon      : '1',
+                    date      : '2019/03/22',
+                    photo     : require('../images/sql/1.png'),
+                    introduce : '致那些我曾喜爱过的一切，虽现在疏散远离，但你们都曾在我的每一个不同的阶段，带给我不一样的欢笑。心存感激，在某个时刻会突然想念，还有地方可以回头看望看望'
+                },
+                {
+                    icon      : '2',
+                    date      : '2019/03/12',
+                    photo     : require('../images/sql/2.png'),
+                    introduce : '致那些我曾喜爱过的一切，虽现在疏散远离，但你们都我的每一个不同的阶段，带给我不一样的欢笑。心存感激，在某个时刻会突然想念，还有地方可以回头看望看望'
+                },
+                {
+                    icon      : '3',
+                    date      : '2019/03/03',
+                    photo     : require('../images/sql/3.png'),
+                    introduce : '致那些我曾喜爱过的一切，虽现在们都曾在我的每一个不同的阶段，带给我不一样的欢笑。心存感激，在某个时刻会突然想念，还有地方可以回头看望看望'
+                },
+                {
+                    icon      : '4',
+                    date      : '2019/02/28',
+                    photo     : require('../images/sql/4.png'),
+                    introduce : '致那些我曾喜爱过的一切，虽现在疏散远同的阶段，带给我不一样的欢笑。心存感激，在某个时刻会突然想念，还有地方可以回头看望看望'
+                },
+                
+            ];
 export default {
 	data () {
 		return {
 		    note: {
-				backgroundImage: "url(" + require("../images/banner.png") + ") ",
+				backgroundImage: "url(" + require("../images/banner1.png") + ") ",
 				backgroundPosition: "center center",
 				backgroundRepeat: "no-repeat",
 				backgroundSize: "cover",
@@ -13,14 +40,36 @@ export default {
 				backgroundRepeat: "no-repeat",
 				backgroundSize: "cover",
 	        },
-			  msg: '溶于世，却恋那山石。<br/>世上太多美好，却折磨着选择。<br/>如果不能选择，那就自己创造一块境地。<br/>依旧恋那山石。为此汇记纯粹的自己。'
+			msg: '溶于世，却恋那山石。<br/>世上太多美好，却折磨着选择。<br/>如果不能选择，那就自己创造一块境地。<br/>依旧恋那山石。为此汇记纯粹的自己。',
+			htmls: [],
+			photo: require('../images/sql/4.png'),
+			img: ''
 		}
 	},
     computed:{
-		
+
 	},
 	methods:{
-		
-	}
+		aaa: function(){
+			alert('aaa');
+			this.$axios.post('/user', {
+			    firstName: 'Fred',
+			    lastName: 'Flintstone'
+			})
+			.then(function (response) {
+				var data = response.data.context.from;
+			    //console.log(response);
+			    console.log(data)
+			})
+			.catch(function (error) {
+			    console.log(error);
+			    alert('aa')
+			});
+		},
+        imgs:function(){
+        this.htmls = arr;
+        }
+	},
+	mounted:function(){this.imgs();},
 
 }
